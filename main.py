8ed074efccc18a8d11e0a38f7189dc958e240ccd
koheipiazza <your.email@example.com>
2025-04-28 11:02:38 +0900

    Update CORS settings with specific Netlify domain

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",  # ローカル開発用
-        "https://*.netlify.app",  # Netlifyのドメイン
+        "https://chiikinoima.netlify.app",  # Netlifyのドメイン
     ],
     allow_credentials=True,
     allow_methods=["*"],
